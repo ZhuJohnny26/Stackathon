2b3d84d324000f2fca55e1a7db2c07a419731a6c
ZhuJohnny26 <johnny.zhu26@gmail.com>
2020-06-26 12:33:09 -0400

    require working on front end

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '82ad3d4ca6da445d89db902db17311ca'
+});
+
+app.models.predict("eeed0b6733a644cea07cf4c60f87ebb7", "https://samples.clarifai.com/metro-north.jpg").then(
+    function(response) {
+      // do something with response
+      console.log(response)
+    },
+    function(err) {
+      // there was an error
+    }
+  );
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test</Text>
       <StatusBar style="auto" />
     </View>
   );
